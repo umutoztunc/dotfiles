dca8dcafee0e76367ac0819709cf31666ad32e95
Umut Barış Öztunç <umutoztunc@gmail.com>
2022-08-01 17:20:43 +0300

    Use awesome for transparency instead of picom.conf

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -568,8 +568,14 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c)
+    c.border_color = beautiful.border_focus
+    c.opacity = 1
+end)
+client.connect_signal("unfocus", function(c)
+    c.border_color = beautiful.border_normal
+    c.opacity = 0.9
+end)
 -- }}}
 
 -- Hide titlebar if the client is floating. Always hide it for Alacritty. {{{
